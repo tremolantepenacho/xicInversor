c26e3568ec336c49c5b53914e1147cbaf1c2a422
lliurex-server <lliurex-server@lliurexserverMS7846>
2016-02-29 11:12:36 +0100

    Antes de hibernate

---
+++ xicInversor/src/vista/VentanaPrincipal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vista;
+
+/**
+ *
+ * @author Héctor Arnau
+ * @fecha 29/02/2016 09:24:06
+ * @company DAM
+ */
+public class VentanaPrincipal {
+    
+    public static JFramePrincipal Ventana;
+
+}
